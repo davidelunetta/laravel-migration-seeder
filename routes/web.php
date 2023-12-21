5f6c32138ebaa05fed4b6a201f1fdd1d14bdf0e5
davidelunetta <davidelunetta.27@gmail.com>
2023-12-21 21:11:33 +0100

    route modified for TrainController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\TrainController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,4 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [TrainController::class,'index']);
