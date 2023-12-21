2c67afb17e68bede211b287132d0d6a7a4569a10
davidelunetta <davidelunetta.27@gmail.com>
2023-12-21 21:12:08 +0100

    TrainController updated

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train;
+use Illuminate\Support\Carbon;
 
 class TrainController extends Controller
 {
-    //
+    public function index()
+{
+    $currentDate = Carbon::now()->toDateString(); // Ottieni la data odierna
+
+    $trains = Train::whereDate('Data_di_partenza', $currentDate)->get();
+    // dd($trains);
+    return view('home', ['trains' => $trains]);
+   
+}
 }
